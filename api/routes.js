dc7a646e1d6a2edec393fb01baf6181ed5d98463
nilesh05apr <nk8588338@gmail.com>
2022-08-17 21:50:55 +0530

    Dockerized

--- api/routes.js
+++ api/routes.js
@@ -2,6 +2,8 @@
 const controller = require("./controller");
 
 module.exports = (app) => {
+    app.route("/health").get(controller.health);
     app.route("/about").get(controller.about);
     app.route("/weather").get(controller.weather);
+
 };
